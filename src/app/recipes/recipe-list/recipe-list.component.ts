b63d088b66ddae33a76a1676d1b4dc0d3acf6839
Anna Zielińska <ankaziele@gmail.com>
2019-09-10 15:27:33 +0200

    adding shopping list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe ('Chicken in Orange souce', 'Food Kitchen Meals Home Made Dishes Recipe Bio', 'http://maxpixel.net/static/photo/1x/Food-Kitchen-Meals-Home-Made-Dishes-Recipe-Bio-1175493.jpg'),
+    new Recipe ('Chicken in Oranges', 'Food Kitchen Meals Home Made Dishes Recipe Bio', 'http://maxpixel.net/static/photo/1x/Food-Kitchen-Meals-Home-Made-Dishes-Recipe-Bio-1175493.jpg'),
     new Recipe ('Mexican Tacos', 'Super crispy, easy to make mexican tacos', 'https://get.pxhere.com/photo/dish-meal-food-salad-cooking-produce-vegetable-kitchen-healthy-cuisine-nutrition-vegan-vegetarian-tacos-diet-organic-ingredients-recipes-remoulade-indian-cuisine-659784.jpg')
   ]
   constructor() { }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = [
+    new Ingredient(1, 'kg', 'Chicken breast'),
+    new Ingredient(500, 'g', 'Oranges')
+  ]
   constructor() { }
 
   ngOnInit() {
