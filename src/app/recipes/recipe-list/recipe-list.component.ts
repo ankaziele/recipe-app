351ea76b9875fcf95ca03cb8f4ae7b7dc69d8de6
ankaziele <ankaziele@gmail.com>
2019-09-10 09:38:39 +0200

    use of model to create blueprint for recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe ('Chicken in Orange souce', 'Food Kitchen Meals Home Made Dishes Recipe Bio', 'http://maxpixel.net/static/photo/1x/Food-Kitchen-Meals-Home-Made-Dishes-Recipe-Bio-1175493.jpg'),
+    new Recipe ('Mexican Tacos', 'Super crispy, easy to make mexican tacos', 'https://get.pxhere.com/photo/dish-meal-food-salad-cooking-produce-vegetable-kitchen-healthy-cuisine-nutrition-vegan-vegetarian-tacos-diet-organic-ingredients-recipes-remoulade-indian-cuisine-659784.jpg')
+  ]
   constructor() { }
 
   ngOnInit() {
